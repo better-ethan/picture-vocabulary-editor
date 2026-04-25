871b0c31a1fbcc6bd940550885539e0d515dcf07
better-ethan <thisisethanlee@gmail.com>
2026-04-25 20:13:47 +0800

    replace toaster withretroui's component

--- web/app/root.tsx
+++ web/app/root.tsx
@@ -9,7 +9,7 @@ import {
 
 import type { Route } from "./+types/root";
 import "@/main.css";
-import { Toaster } from "@/components/ui/sonner";
+import { Toaster } from "@/components/retroui/Sonner";
 import { TRPCProvider, trpc } from "@/util";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { cn } from "@/lib/utils";
