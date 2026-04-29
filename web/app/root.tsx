a499c8d6df3311789576a7e30baf73731c2dcf7c
better-ethan <thisisethanlee@gmail.com>
2026-04-29 14:30:09 +0800

    add navigation bar

--- web/app/root.tsx
+++ web/app/root.tsx
@@ -1,5 +1,6 @@
 import {
   isRouteErrorResponse,
+  Link,
   Links,
   Meta,
   Outlet,
@@ -13,6 +14,10 @@ import { Toaster } from "@/components/retroui/Sonner";
 import { TRPCProvider, trpc } from "@/util";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { cn } from "@/lib/utils";
+import { Text } from "@/components/retroui/Text";
+import { Button } from "@/components/retroui/Button";
+import { Drawer } from "@/components/retroui/Drawer";
+import { HamburgerIcon, MenuIcon } from "lucide-react";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -27,6 +32,8 @@ export const links: Route.LinksFunction = () => [
   },
 ];
 
+const baseDrawerLinkClasses =
+  "font-bold text-black decoration-2 underline-offset-4 transition-colors hover:text-[#3B82F6] hover:underline";
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
@@ -41,6 +48,73 @@ export function Layout({ children }: { children: React.ReactNode }) {
           "absolute inset-0 h-full w-full bg-white bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-size-[24px_24px]"
         )}
       >
+        <nav className="relative sticky top-0 z-50 border-b-2 border-black bg-white px-4 py-4 md:px-8">
+          <div className="mx-auto flex max-w-7xl items-center justify-between">
+            <Link className="flex cursor-pointer items-center gap-2" to="/">
+              <Text className="text-2xl text-black font-display font-black uppercase">
+                Picture Lesson
+              </Text>
+            </Link>
+            <div className="hidden items-center gap-8 md:flex">
+              <Link
+                className="font-bold text-black decoration-2 underline-offset-4 transition-colors hover:text-[#3B82F6] hover:underline"
+                to="/how-it-works"
+              >
+                How it works
+              </Link>
+              <Link
+                className=" font-bold text-black decoration-2 underline-offset-4 transition-colors hover:text-[#3B82F6] hover:underline"
+                to="/picture-lesson/list"
+              >
+                Lessons
+              </Link>
+              <Link
+                className=" font-bold text-black decoration-2 underline-offset-4 transition-colors hover:text-[#3B82F6] hover:underline"
+                to="/pricing"
+              >
+                Pricing
+              </Link>
+              <div className="ml-2 flex items-center gap-3">
+                <Button asChild variant="default" size="sm">
+                  <Link to="/signin" className="uppercase">
+                    Log in
+                  </Link>
+                </Button>
+              </div>
+            </div>
+            <div className="flex items-center gap-3 md:hidden">
+              <Drawer direction="right">
+                <Drawer.Trigger asChild>
+                  <Button size="icon">
+                    <MenuIcon />
+                  </Button>
+                </Drawer.Trigger>
+                <Drawer.Content>
+                  <div className="flex h-full flex-col gap-8 p-4">
+                    <Link
+                      to="/how-it-works"
+                      className={cn(baseDrawerLinkClasses)}
+                    >
+                      How it works
+                    </Link>
+                    <Link
+                      to="/picture-lesson/list"
+                      className={cn(baseDrawerLinkClasses)}
+                    >
+                      Lessons
+                    </Link>
+                    <Link to="/pricing" className={cn(baseDrawerLinkClasses)}>
+                      Pricing
+                    </Link>
+                    <Link to="/signin" className={cn(baseDrawerLinkClasses)}>
+                      Log in
+                    </Link>
+                  </div>
+                </Drawer.Content>
+              </Drawer>
+            </div>
+          </div>
+        </nav>
         <Toaster position="top-center" />
         {children}
         <ScrollRestoration />
