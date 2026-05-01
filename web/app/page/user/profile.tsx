a38257c1161a00ffd21ee01bebb8cc3bba19cdd8
better-ethan <thisisethanlee@gmail.com>
2026-05-01 12:04:55 +0800

    update nickname

--- web/app/page/user/profile.tsx
+++ web/app/page/user/profile.tsx
@@ -1,15 +1,38 @@
+import { Button } from "@/components/retroui/Button";
 import {
   Card,
   CardContent,
   CardHeader,
   CardTitle,
 } from "@/components/retroui/Card";
+import { Input } from "@/components/retroui/Input";
 import { authClient } from "@/lib/auth-client";
+import { CheckIcon, PencilIcon, XIcon } from "lucide-react";
+import { useState } from "react";
+import { toast } from "sonner";
 
 export const loader = async () => {};
 
 export default function Page() {
   const { data: session } = authClient.useSession();
+
+  const [editingNickname, setEditingNickname] = useState(false);
+  const [nickname, setNickname] = useState(session?.user.name || "");
+
+  const handleSave = async () => {
+    try {
+      const result = await authClient.updateUser({ name: nickname });
+      setEditingNickname(false);
+      if (!result.error) {
+        toast.success("Nickname updated successfully!");
+      }
+    } finally {
+    }
+  };
+
+  const handleCancel = () => {
+    setEditingNickname(false);
+  };
 
   return (
     <div className="max-w-lg min-w-xs mx-auto px-4">
@@ -23,9 +46,48 @@ export default function Page() {
               <span className="font-medium">Email</span>
               <span className="text-gray-400">{session?.user.email}</span>
             </div>
-            <div className="flex justify-between">
+            <div className="flex justify-between items-center gap-2">
               <span className="font-medium">Nickname</span>
-              <span className="text-gray-400">{session?.user.name}</span>
+              {editingNickname ? (
+                <div className="flex items-center gap-2">
+                  <Input
+                    autoFocus
+                    value={nickname}
+                    className="h-7 w-36"
+                    onChange={(e) => setNickname(e.target.value)}
+                    onKeyDown={(e) => {
+                      if (e.key === "Enter") {
+                        handleSave();
+                      } else if (e.key === "Escape") {
+                        handleCancel();
+                      }
+                    }}
+                  />
+                  <Button size="icon" variant="ghost" onClick={handleSave}>
+                    <CheckIcon
+                      className="size-4 text-green-500"
+                      strokeWidth={4}
+                    />
+                  </Button>
+                  <Button size="icon" variant="ghost" onClick={handleCancel}>
+                    <XIcon className="size-4 text-red-500" strokeWidth={4} />
+                  </Button>
+                </div>
+              ) : (
+                <div className="flex items-center gap-2">
+                  <span className="text-gray-400">{session?.user.name}</span>
+                  <Button
+                    size="icon"
+                    variant="ghost"
+                    onClick={() => {
+                      setEditingNickname(true);
+                      setNickname(session?.user.name || "");
+                    }}
+                  >
+                    <PencilIcon className="size-4" />
+                  </Button>
+                </div>
+              )}
             </div>
           </div>
         </CardContent>
