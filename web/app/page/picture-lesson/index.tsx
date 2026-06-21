5c5d8b6882f9b89827640aa79a2a7dabfaf44683
better-ethan <thisisethanlee@gmail.com>
2026-06-21 23:06:31 +0800

    implement fill-in feature

--- web/app/page/picture-lesson/index.tsx
+++ web/app/page/picture-lesson/index.tsx
@@ -29,6 +29,7 @@ import {
   SelectValue,
 } from "@/components/retroui/Select";
 import { Field } from "@/components/ui/field";
+import { Input } from "@/components/retroui/Input";
 
 export const loader = async ({ params, request }: Route.LoaderArgs) => {
   const id = params.id;
@@ -57,6 +58,8 @@ export const loader = async ({ params, request }: Route.LoaderArgs) => {
   return result;
 };
 
+type Mode = "view" | "fillIn" | "dictation" | "wordBuilding";
+
 export default function Page() {
   const data = useLoaderData<typeof loader>();
 
@@ -64,6 +67,34 @@ export default function Page() {
     data.content as unknown as CanvasContent;
 
   const [rate, setRate] = useState(1);
+
+  const [mode, setMode] = useState<Mode>("view");
+
+  const [userAnswers, setUserAnswers] = useState<string[]>([]);
+  const [userAnswerResults, setUserAnswerResults] = useState<boolean[]>([]);
+
+  const [isCheckedAnswer, setIsCheckedAnswer] = useState(false);
+
+  const handleFillIn = () => {
+    setMode("fillIn");
+    setUserAnswers(Array(words.length).fill(""));
+    setUserAnswerResults(Array(words.length).fill(false));
+
+    setIsCheckedAnswer(false);
+  };
+
+  const handleDictation = () => {};
+
+  const handleWordBuilding = () => {};
+
+  const handleCheckAnswer = () => {
+    const results = words.map((word, index) => {
+      const userAnswer = userAnswers[index] || "";
+      return userAnswer.trim().toLowerCase() === word.word.trim().toLowerCase();
+    });
+    setUserAnswerResults(results);
+    setIsCheckedAnswer(true);
+  };
 
   return (
     <div className="flex flex-col items-center">
@@ -84,60 +115,122 @@ export default function Page() {
           />
 
           <Card className="lg:w-64">
-            <CardContent className="p-2">
-              <CardTitle className="text-lg lg:text-2xl">Word List</CardTitle>
-              <Field className="flex flex-row justify-between items-center mb-4">
-                <Label htmlFor="rate">Rate</Label>
-                <Select
-                  value={rate.toString()}
-                  onValueChange={(value) => setRate(parseFloat(value))}
-                >
-                  <SelectTrigger
-                    id="rate"
-                    className="min-w-12 h-8 p-2 py-1 shadow-none"
+            <CardContent className="flex flex-col justify-between p-2 h-full">
+              <div>
+                <CardTitle className="text-lg lg:text-2xl">Word List</CardTitle>
+                <Field className="flex flex-row justify-between items-center mb-4">
+                  <Label htmlFor="rate">Rate</Label>
+                  <Select
+                    value={rate.toString()}
+                    onValueChange={(value) => setRate(parseFloat(value))}
                   >
-                    <SelectValue />
-                  </SelectTrigger>
-                  <SelectContent className="w-12">
-                    <SelectItem value="0.5">🐢 0.5x</SelectItem>
-                    <SelectItem value="0.75">🚶 0.75x</SelectItem>
-                    <SelectItem value="1">🚴 1x</SelectItem>
-                    <SelectItem value="1.25">🏃 1.25x</SelectItem>
-                    <SelectItem value="1.5">🚗 1.5x</SelectItem>
-                    <SelectItem value="1.75">✈️ 1.75x</SelectItem>
-                    <SelectItem value="2">🚀 2x</SelectItem>
-                  </SelectContent>
-                </Select>
-              </Field>
-              <ul className="flex flex-col gap-2 pr-2 overflow-y-auto max-h-40 lg:max-h-full">
-                {words.map(({ number, word, audio }) => (
-                  <li key={number} className="flex items-center gap-2">
-                    <Badge
-                      variant="default"
-                      className={cn(
-                        "w-6 h-6 flex items-center justify-center shrink-0 rounded-full p-0",
-                        "bg-white text-gray-700 border"
+                    <SelectTrigger
+                      id="rate"
+                      className="min-w-12 h-8 p-2 py-1 shadow-none"
+                    >
+                      <SelectValue />
+                    </SelectTrigger>
+                    <SelectContent className="w-12">
+                      <SelectItem value="0.5">🐢 0.5x</SelectItem>
+                      <SelectItem value="0.75">🚶 0.75x</SelectItem>
+                      <SelectItem value="1">🚴 1x</SelectItem>
+                      <SelectItem value="1.25">🏃 1.25x</SelectItem>
+                      <SelectItem value="1.5">🚗 1.5x</SelectItem>
+                      <SelectItem value="1.75">✈️ 1.75x</SelectItem>
+                      <SelectItem value="2">🚀 2x</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </Field>
+                {mode === "fillIn" && (
+                  <Text className="text-sm text-gray-500 my-2">
+                    Fill in the blanks, then click "Check" to verify your
+                    answers.
+                  </Text>
+                )}
+                <ul className="flex flex-col gap-2 pr-2 overflow-y-auto max-h-40 lg:max-h-full">
+                  {words.map(({ number, word, audio }, index) => (
+                    <li key={number} className="flex items-center gap-2">
+                      <Badge
+                        variant="default"
+                        className={cn(
+                          "w-6 h-6 flex items-center justify-center shrink-0 rounded-full p-0",
+                          "bg-white text-gray-700 border"
+                        )}
+                      >
+                        {number}
+                      </Badge>
+                      {mode === "fillIn" && (
+                        <Input
+                          type="text"
+                          value={userAnswers[index] || ""}
+                          onChange={(e) => {
+                            const newAnswers = [...userAnswers];
+                            newAnswers[index] = e.target.value;
+                            setUserAnswers(newAnswers);
+                          }}
+                          className={cn(
+                            "flex-1 border rounded px-2 py-1",
+                            userAnswerResults[index] === true &&
+                              isCheckedAnswer &&
+                              "border-green-500 bg-green-100",
+                            userAnswerResults[index] === false &&
+                              isCheckedAnswer &&
+                              "border-red-600 bg-red-300"
+                          )}
+                        />
                       )}
-                    >
-                      {number}
-                    </Badge>
-                    <Text className="flex-1 font-normal">{word}</Text>
-                    <Button
-                      type="button"
-                      variant="outline"
-                      size="sm"
-                      disabled={!word}
-                      onClick={async () => {
-                        await playAudio(audio, rate);
-                      }}
-                    >
-                      <Volume2 className="w-4 h-4" />
-                    </Button>
-                  </li>
-                ))}
-              </ul>
+                      {mode === "view" && (
+                        <Text className="flex-1 font-normal">{word}</Text>
+                      )}
+                      <Button
+                        type="button"
+                        variant="outline"
+                        size="sm"
+                        disabled={!word}
+                        onClick={async () => {
+                          await playAudio(audio, rate);
+                        }}
+                      >
+                        <Volume2 className="w-4 h-4" />
+                      </Button>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              {mode === "fillIn" && (
+                <div className="flex justify-between">
+                  <Button
+                    size="sm"
+                    variant="outline"
+                    onClick={() => {
+                      setMode("view");
+                    }}
+                  >
+                    Exit
+                  </Button>
+                  <Button size="sm" onClick={handleCheckAnswer}>
+                    Check
+                  </Button>
+                </div>
+              )}
             </CardContent>
           </Card>
+        </div>
+        <div className="flex gap-2 justify-end w-full px-2">
+          <Button
+            type="button"
+            size="sm"
+            variant={"secondary"}
+            onClick={handleFillIn}
+          >
+            Fill in
+          </Button>
+          <Button type="button" size="sm" variant={"secondary"}>
+            Dictation
+          </Button>
+          <Button type="button" size="sm" variant={"secondary"}>
+            Word Building
+          </Button>
         </div>
         <div className="flex flex-col gap-2">
           <h1 className="text-2xl font-bold text-start">{data.title}</h1>
