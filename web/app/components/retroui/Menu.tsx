486f995a119ca74aa3b105758cd811724348456a
better-ethan <thisisethanlee@gmail.com>
2026-06-29 23:16:19 +0800

    upgrade Menu component for React 19

--- web/app/components/retroui/Menu.tsx
+++ web/app/components/retroui/Menu.tsx
@@ -2,40 +2,44 @@
 
 import { cn } from "@/lib/utils";
 import { Menu as BaseMenu } from "@base-ui/react/menu";
-import React, { ComponentPropsWithoutRef } from "react";
+import React from "react";
 
 const Menu = BaseMenu.Root;
 const Trigger = BaseMenu.Trigger;
 
-interface IMenuContent
-  extends ComponentPropsWithoutRef<typeof BaseMenu.Popup> {}
-
-const Content = ({ className, ...props }: IMenuContent) => (
+const Content = ({
+  className,
+  ...props
+}: React.ComponentProps<typeof BaseMenu.Popup>) => (
   <BaseMenu.Portal>
-    <BaseMenu.Popup
-      className={cn(
-        "bg-white border-2 shadow-md absolute top-2 min-w-20",
-        className,
-      )}
-      {...props}
-    />
+    <BaseMenu.Positioner>
+      <BaseMenu.Popup
+        className={cn(
+          "bg-white border-2 shadow-md absolute top-2 min-w-40 z-50",
+          className
+        )}
+        {...props}
+      />
+    </BaseMenu.Positioner>
   </BaseMenu.Portal>
 );
 
-const MenuItem = React.forwardRef<
-  HTMLDivElement,
-  ComponentPropsWithoutRef<typeof BaseMenu.Item>
->(({ className, ...props }, ref) => (
-  <BaseMenu.Item
-    ref={ref}
-    className={cn(
-      "relative text-black flex cursor-default select-none items-center rounded-xs px-2 py-1.5 text-sm outline-hidden transition-colors hover:bg-primary focus:bg-primary data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
-      className,
-    )}
-    {...props}
-  />
-));
-MenuItem.displayName = "MenuItem";
+function MenuItem({
+  className,
+  ref,
+  ...props
+}: React.ComponentProps<typeof BaseMenu.Item>) {
+  return (
+    <BaseMenu.Item
+      ref={ref}
+      className={cn(
+        "relative text-black flex cursor-default select-none items-center rounded-xs px-2 py-1.5 text-sm outline-hidden transition-colors hover:bg-primary focus:bg-primary data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+        className
+      )}
+      {...props}
+    />
+  );
+}
 
 const MenuComponent = Object.assign(Menu, {
   Trigger,
