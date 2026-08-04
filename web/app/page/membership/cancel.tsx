50f33697ae6a418fa55c859e2d4b2ce337e6fee4
better-ethan <thisisethanlee@gmail.com>
2026-08-04 18:27:10 +0800

    implement pricing cancel page

--- web/app/page/membership/cancel.tsx
+++ web/app/page/membership/cancel.tsx
@@ -1,3 +1,48 @@
+import { Button } from "@/components/ui/Button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/Card";
+import { XCircleIcon } from "lucide-react";
+import { Link } from "react-router";
+
 export default function Page() {
-  return <div></div>;
+  return (
+    <div className="flex items-center justify-center">
+      <Card className="w-full max-w-md text-center shadow-sm">
+        <CardHeader>
+          <div className="flex justify-center mb-2">
+            <XCircleIcon className="h-12 w-12 text-red-500" />
+          </div>
+          <CardTitle className="text-2xl font-bold">
+            Payment Cancelled
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p className="text-muted-foreground">
+            Your payment was cancelled and you have not been charged.
+          </p>
+          <p className="text-muted-foreground">
+            If you have any questions or need help, feel free to contact our
+            support team.
+          </p>
+        </CardContent>
+        <CardFooter className="flex flex-col gap-4 bg-inherit border-none">
+          <Button
+            variant="secondary"
+            className="w-full shadow-sm"
+            render={<Link to="/pricing">Back to Pricing</Link>}
+          ></Button>
+          <Button
+            variant="outline"
+            className="w-full shadow-sm"
+            render={<Link to="/">Go to Home</Link>}
+          ></Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 }
